9760252824e9f836bc4be44075aeca9303236777
tryashtar <tryashtar@gmail.com>
2022-04-04 15:53:09 -0600

    Add project files.

---
+++ ShulkerBundle/MainForm.cs
@@ -0,0 +1,14 @@
+namespace ShulkerBundle;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+
+    }
+}
